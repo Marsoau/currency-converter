569e10d27dacd114f848aff543b398fd18227df9
Marsoau <koteika5063@gmail.com>
2024-09-09 22:33:04 +0300

    App component cleanup and CurrencyInput component addition

---
+++ src/app/currency-input/currency-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-input.component.html',
+  styleUrl: './currency-input.component.scss'
+})
+export class CurrencyInputComponent {
+
+}
